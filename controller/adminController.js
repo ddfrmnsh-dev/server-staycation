7ab812ef970fbe059b7e55904992d0c0608db853
Dede <dede@cakravala.id>
2023-08-09 20:43:25 +0700

    update

--- controller/adminController.js
+++ controller/adminController.js
@@ -2,6 +2,7 @@ const Category = require("../models/Category");
 const Bank = require("../models/Bank");
 const Item = require("../models/Item");
 const Image = require("../models/Image");
+const Feature = require("../models/Feature");
 const fs = require("fs-extra");
 const path = require("path");
 
@@ -333,7 +334,53 @@ module.exports = {
       console.log(e);
       req.flash("alertMessage", `${e.message}`);
       req.flash("alertStatus", "danger");
-      res.redirect("/admin/item");
+    }
+  },
+  viewDetailItem: async (req, res) => {
+    try {
+      const { itemId } = req.params;
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = { message: alertMessage, status: alertStatus };
+      const feature = await Feature.find({ itemId: itemId });
+      console.log(feature);
+      res.render("admin/item/detail_item/viewDetailItem", {
+        title: "Staycation | Detail item",
+        alert,
+        itemId,
+        feature,
+      });
+    } catch (e) {
+      req.flash("alertMessage", `${e.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    }
+  },
+  addFeature: async (req, res) => {
+    const { name, qty, itemId } = req.body;
+    try {
+      if (!req.file) {
+        req.flash("alertMessage", "Image not found");
+        req.flash("alertStatus", "danger");
+        res.redirect(`/admin/item/show-detail-item/${itemId}`);
+      }
+      const feature = await Feature.create({
+        name,
+        qty,
+        itemId,
+        imageUrl: `images/${req.file.filename}`,
+      });
+
+      const item = await Item.findOne({ _id: itemId });
+      item.featureId.push({ _id: feature.id });
+      await item.save();
+      req.flash("alertMessage", "Success Add Feature");
+      req.flash("alertStatus", "success");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
+    } catch (e) {
+      req.flash("alertMessage", `${e.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect(`/admin/item/show-detail-item/${itemId}`);
     }
   },
   viewBooking: (req, res) => {
--- routes/admin.js
+++ routes/admin.js
@@ -22,6 +22,9 @@ router.get("/item/:id", adminController.showEditItem);
 router.put("/item/:id", uploadMultiple, adminController.editItem);
 router.delete("/item/:id/delete", adminController.deleteItem);
 
+router.get("/item/show-detail-item/:itemId", adminController.viewDetailItem);
+router.post("/item/add/feature", uploadSingle, adminController.addFeature);
+
 router.get("/booking", adminController.viewBooking);
 
 module.exports = router;
